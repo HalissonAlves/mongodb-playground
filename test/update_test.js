936af118a0c7b7e65879637142d2e1e432f1e50d
Halisson Alves <halisson.eng@gmail.com>
2025-02-08 19:20:09 -0300

    feat: create update file with some update functions to use with mongoose

---
+++ test/update_test.js
@@ -0,0 +1,42 @@
+import assert from 'assert';
+import User from '../src/user.js';
+
+describe('Updating records', () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe', likes: 0 });
+    joe.save().then(() => done());
+  });
+
+  it('instance type using set n save', (done) => {
+    joe.set('name', 'Alex');
+    joe.save().then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+        done();
+      });
+  });
+
+  it('A model instance can update', (done) => {
+    joe.updateOne({ name: 'Alex' })
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+        done();
+      });
+  });
+
+  it('A model class can update', (done) => {
+    User.updateMany({ name: 'Joe' }, { name: 'Alex' })
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+        done();
+      });
+  });
+
+});
