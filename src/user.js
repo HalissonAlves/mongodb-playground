e545d42c89292adcf2c76f2627422158d97f5c09
Halisson Alves <halisson.eng@gmail.com>
2025-02-08 19:43:00 -0300

    feat: create validation tests

--- src/user.js
+++ src/user.js
@@ -2,7 +2,14 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, 'Name is required.'],
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters.'
+    },
+  },
   postCount: Number
 }, { timestamps: true });
 
