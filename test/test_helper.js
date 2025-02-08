bd0e8942f885e5535803d3ad9d36971c4536913e
Halisson Alves <halisson.eng@gmail.com>
2025-02-08 13:09:55 -0300

    feat: create project initial structure

---
+++ test/test_helper.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(`mongodb://${process.env.DB_HOST}/users_test`);
+mongoose.connection
+  .once('open', () => console.log('Good to go!'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
